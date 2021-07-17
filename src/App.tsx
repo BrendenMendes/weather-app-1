da9cf17c9977f26c1fb3d31a4c19c74ce65c707a
BrendenMendes <30852885+BrendenMendes@users.noreply.github.com>
2021-07-17 09:42:15 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { LocationSearch } from "./LocationSearch";
+import { LocationTable } from "./LocationTable";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [locations, setLocations] = useState<string[]>([]);
+    const addLocation = (location: string) => setLocations([location, ...locations]);
+
+    return (
+        <div className="container">
+            <h1>Weather App</h1>
+            <LocationSearch onSearch={addLocation}/>
+            <LocationTable locations={locations}/>
+        </div>
+    );
 }
 
 export default App;
